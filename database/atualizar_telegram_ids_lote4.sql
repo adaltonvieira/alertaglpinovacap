caf898798f355ab87df0f5bfb9ed025f8b24bafe
Adalton <adalton.v.silva@gmail.com>
2026-08-04 15:25:51 -0300

    Registra ultimos 5 telegram_chat_id e desativa 3 tecnicos que nao vao entrar

---
+++ database/atualizar_telegram_ids_lote4.sql
@@ -0,0 +1,9 @@
+UPDATE tecnicos SET telegram_chat_id=1904974406 WHERE glpi_user_id=3671;
+UPDATE tecnicos SET telegram_chat_id=1134161934 WHERE glpi_user_id=3218;
+UPDATE tecnicos SET telegram_chat_id=8607267061 WHERE glpi_user_id=3858;
+UPDATE tecnicos SET telegram_chat_id=953820270  WHERE glpi_user_id=3408;
+UPDATE tecnicos SET telegram_chat_id=1020459901 WHERE glpi_user_id=3703;
+
+UPDATE tecnicos SET ativo=0 WHERE glpi_user_id=1256;
+UPDATE tecnicos SET ativo=0 WHERE glpi_user_id=2923;
+UPDATE tecnicos SET ativo=0 WHERE glpi_user_id=1141;
